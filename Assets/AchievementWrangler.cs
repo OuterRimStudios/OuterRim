c51898c776e06934fa8b09556d99ea0be1cd7d88
Borgeshc <hborges@digitaliris.com>
2016-10-27 17:16:40 -0500

    Started work on achievement menu

---
+++ Assets/AchievementWrangler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementWrangler : MonoBehaviour
+{
+    public GameObject[] mainAchieves;
+    public GameObject[] subAchieves;
+    float vertical;
+       
+	void Update ()
+    {
+	}
+}
---
+++ Assets/CauseBlur.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CauseBlur : MonoBehaviour
+{
+    UnityStandardAssets.ImageEffects.CameraMotionBlur blur;
+
+    void Start()
+    {
+        blur = Camera.main.GetComponent<UnityStandardAssets.ImageEffects.CameraMotionBlur>();
+    }
+
+    public void StartBlur()
+    {
+        StartCoroutine(Blur());
+    }
+    public void EndBlur()
+    {
+        StartCoroutine(UnBlur());
+    }
+    IEnumerator Blur()
+    {
+        for(int i = 0; i < 500; i = i + 10)
+        {
+            yield return new WaitForSeconds(.01f);
+            blur.velocityScale++;
+        }
+    }
+
+    IEnumerator UnBlur()
+    {
+        for (int i = 0; i < 500; i = i + 10)
+        {
+            yield return new WaitForSeconds(.01f);
+            blur.velocityScale--;
+        }
+    }
+}
