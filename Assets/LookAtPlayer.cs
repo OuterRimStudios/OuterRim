57fe0d671527822b293dcf2f23aa425e40dfdf71
Borgeshc <hborges@digitaliris.com>
2016-12-07 12:49:01 -0600

    Added lookat for reticle

---
+++ Assets/LookAtPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	void Update () {
+        transform.LookAt(player.transform);
+	}
+}
