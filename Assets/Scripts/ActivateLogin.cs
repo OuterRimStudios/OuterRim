5a4300198becfbee4e7d5d1ba156e783f8425cef
Sam <sam@defendtheouterrim.com>
2016-12-09 19:00:18 -0600

    added gamejolt login

---
+++ Assets/Scripts/ActivateLogin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateLogin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ActivateLoginMenu()
+    {
+        GameJolt.UI.Manager.Instance.ShowSignIn();
+    }
+}
