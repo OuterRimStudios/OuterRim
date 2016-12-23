e9fe2431b5370761546cad30b196e3f7496d4de8
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-12-22 22:32:32 -0600

    updated lock out dates

--- Assets/Scripts/BetaCheckDate.cs
+++ Assets/Scripts/BetaCheckDate.cs
@@ -11,14 +11,14 @@ public class BetaCheckDate : MonoBehaviour
     void Start()
     {
         string[] playableDates = new string[8];
-        playableDates[0] = "12/15/2016";
-        playableDates[1] = "12/16/2016";
-        playableDates[2] = "12/17/2016";
-        playableDates[3] = "12/18/2016";
-        playableDates[4] = "12/19/2016";
-        playableDates[5] = "12/20/2016";
-        playableDates[6] = "12/21/2016";
-        playableDates[7] = "12/22/2016";
+        playableDates[0] = "12/22/2016";
+        playableDates[1] = "12/23/2016";
+        playableDates[2] = "12/24/2016";
+        playableDates[3] = "12/25/2016";
+        playableDates[4] = "12/26/2016";
+        playableDates[5] = "12/27/2016";
+        playableDates[6] = "12/28/2016";
+        playableDates[7] = "12/29/2016";
 
         foreach (string date in playableDates)
         {
