419b7e59f1e75dd1d4dfc0e4247f12ecdac9df4c
Sam <sam@defendtheouterrim.com>
2017-03-16 08:25:43 -0500

    added steamworks.net package. added the steammanager to the main menu

---
+++ Assets/Scripts/Utility/SteamScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Steamworks;
+
+public class SteamScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    if(SteamManager.Initialized)
+        {
+            string name = SteamFriends.GetPersonaName();
+            Debug.Log(name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
